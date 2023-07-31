7d2c2455d7b34a4e1403213d1037b0f3c991fe43
Wendry bergamasco <2022102020033@aluno.ifro.edu.br>
2023-07-31 16:47:25 -0400

    exemplo2POO

---
+++ exemplo2POO/Aluno.cs
@@ -0,0 +1,19 @@
+﻿public class Aluno
+{
+    public int Matricula { get; set; }
+    public String nome { get; set; }
+    public string CPF { get; set; }
+    public DateOnly dataNascimento { get; set; }
+
+    public Aluno()
+    {
+
+    }
+    public Aluno(int matricula, string nome, string cpf, DateOnly dataNascimento)
+    {
+        this.Matricula = matricula;
+        this.nome = nome;
+        this.CPF = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+}
---
+++ exemplo2POO/Program.cs
@@ -0,0 +1,14 @@
+﻿Aluno aluno1 = new Aluno();
+aluno1.nome = "Elias";
+aluno1.CPF = "222";
+
+Aluno aluno2 = new Aluno(11, "Elias", "06578932267", new DateOnly(2006, 04, 05));
+
+List<Aluno> alunos = new List<Aluno>();
+alunos.Add(aluno1);
+alunos.Add(aluno2);
+
+foreach(Aluno a in alunos)
+{
+    Console.WriteLine("o nome é " + a.nome + " e o cpf é " + a.CPF);
+}
